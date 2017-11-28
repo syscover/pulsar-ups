451d88bc1faf77cf47c62e960a83fd5baeb12cb9
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-11-28 13:08:13 +0100

    config

--- src/config/pulsar-ups.php
+++ src/config/pulsar-ups.php
@@ -19,14 +19,22 @@ return [
 
     'country_codes' => [
         'ES' => [
-            '35***' => 'IC',
-            '38***' => 'IC',
+            '35***' => 'IC',    // Islas Canarias
+            '38***' => 'IC',    // Islas Canarias
+            '51***' => 'XC',    // Ceuta
+            '52***' => 'XL',    // Melilla
         ]
     ],
 
     'services' => [
         'IC' => [
-            '65' => ['code' => '65',    'name' =>'UPS Express Saver',                  'saver' => true]
+            ['code' => '65',    'name' =>'UPS Express Saver',                  'saver' => true]
+        ],
+        'XC' => [
+            ['code' => '65',    'name' =>'UPS Express Saver',                  'saver' => true]
+        ],
+        'XL' => [
+            ['code' => '65',    'name' =>'UPS Express Saver',                  'saver' => true]
         ],
         'US' => [
             ['code' => '07',    'name' =>'UPS Express',                        'saver' => false],
@@ -34,6 +42,13 @@ return [
             ['code' => '54',    'name' =>'UPS Express Plus',                   'saver' => false],
             ['code' => '65',    'name' =>'UPS Express Saver',                  'saver' => true],
             ['code' => '96',    'name' =>'UPS Worldwide Express Freight',      'saver' => false]
-        ]
+        ],
+        'CA' => [
+            ['code' => '07',    'name' =>'UPS Express',                        'saver' => false],
+            ['code' => '08',    'name' =>'UPS Expedited',                      'saver' => false],
+            ['code' => '54',    'name' =>'UPS Express Plus',                   'saver' => false],
+            ['code' => '65',    'name' =>'UPS Express Saver',                  'saver' => true],
+            ['code' => '96',    'name' =>'UPS Worldwide Express Freight',      'saver' => false]
+        ],
     ],
 ];
